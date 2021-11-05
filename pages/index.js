c2e893c5f53881048ef475639dfdd4a1c272a6aa
michaelsatumba <mjsatumba@gmail.com>
2021-11-05 16:07:11 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import tw from "tailwind-styled-components"
+import Map from './components/Map.js'
+
+
+
+export default function Home() {
+
+  return (
+    <Wrapper>
+      <Map />
+      <ActionItems>Start</ActionItems>
+    </Wrapper>
+  )
+}
+
+const Wrapper = tw.div`
+flex flex-col bg-red-300 h-screen
+`
+
+
+const ActionItems = tw.div`
+flex-1
+`
